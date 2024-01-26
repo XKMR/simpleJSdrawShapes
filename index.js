b82fe169718eaef558ec89f24f89b5a61099dc84
kamyar <58393719+XKMR@users.noreply.github.com>
2024-01-27 01:30:17 +0330

    Create index.js

---
+++ index.js
@@ -0,0 +1,49 @@
+const SPACECHAR = '   ';
+const BLOCKCHAR = '[ ]';
+const VERWALL = 1000;
+const HORWALL = 1000;
+
+var square = { position: [1, 1], width: 2, height:2, blocks: [] };
+square.blocks.push(squareBlocks(square.position, square.width, square.height));
+
+
+function squareBlocks(square, width, height) {
+  var blocks = [];
+  console.log()
+  for (var blockY = 0; blockY <= VERWALL ; blockY += 1) {
+    for (var blockX = 0; blockX <= HORWALL ; blockX += 1) {
+      if(blockY >= square[1] && blockY < square[1] + height && blockX >= square[0] && blockX < square[0] + width) {
+        blocks.push([blockX, blockY]);
+        }
+    }
+  }
+  return blocks;
+}
+
+function draw(position) {
+  var plot = '';
+  for (var y = 0; y <= VERWALL; y += 1) {
+    for (var x = 0; x <= HORWALL; x += 1) {
+      if (nestedIncludes(position, x, y)) {
+        plot += BLOCKCHAR;
+      } else {
+        plot += SPACECHAR;
+      }
+    }
+    plot += '\n';
+  }
+  return plot;
+}
+
+function nestedIncludes(arr, x, y) {
+  var doesInclude = false;
+  for (var i = 0; i < arr.length; i += 1) {
+    if (arr[i][0] == x && arr[i][1] == y) {
+      doesInclude = true;
+    }
+  }
+  return doesInclude;
+}
+
+console.log(draw(square.blocks.flat()));
+//console.log(square.blocks)
